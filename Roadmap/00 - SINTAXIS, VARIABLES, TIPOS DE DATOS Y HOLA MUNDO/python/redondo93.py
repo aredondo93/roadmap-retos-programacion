55d8466a807d6e2916adbf371de94500577e9977
aredondo93 <79773025+aredondo93@users.noreply.github.com>
2024-07-18 13:13:44 -0500

    Create redondo93.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/redondo93.py
@@ -0,0 +1,49 @@
+# https://python.org/
+
+# Sintaxis diferentes para los comentarios
+
+# Esto es un comentario de una sola línea 
+
+"""
+Esto es un comentario de varias líneas
+Puedo escribir múltiples líneas de texto aquí.
+"""
+
+'''
+Esto también es un comentario de varias líneas.
+Pueden usar comillas simples o dobles triples.
+'''
+
+# Declaración y asignación de variables
+"""
+Para declarar una variable en Python, simplemente le
+asignas un valor usando el signo '=', son dinámicamente tipadas,
+es decir no necesitas declarar el tipo de variable explícitamente
+"""
+nombre = 'Alberto'
+edad = 25
+altura = 1.75
+
+# Declaración de constantes por convención
+'''
+Paython no tiene una forma nativa de declarar constantes, sin embargo,
+por convención, los nombres de las constantes se escriben en mayúsculas
+'''
+PI = 3.14159
+GRAVEDAD = 9.81
+
+# Variables de datos primitivos
+# Números enteros (int)
+edad = 25
+# Números flotantes (float)
+Precio = 19.99
+# Cadenas de texto (str)
+nombre = "Ana"
+# Booleanos (bool)
+es_estudiante = True
+es_mayor_de_edad = False
+
+#imprimir
+nombre_lenguaje = "Python"
+print("¡Hola", nombre_lenguaje,"!")
+
